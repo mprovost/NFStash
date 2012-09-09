449740e29f4cc0abc2162a7d1a756eae1503eee0
Matt Provost <mprovost@wetafx.co.nz>
2012-09-09 18:51:43 +1200

    more defs for mounting

--- src/nfsping.h
+++ src/nfsping.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <rpc/types.h>
+#include <rpc/xdr.h>
 #include <rpc/rpc.h>
 #include <time.h>
 #include <signal.h>
@@ -22,7 +24,22 @@
 #define NFS_SLEEP { 1, 0 };
 
 /* this isn't really a standard */
-#define MOUNT_PORT    4046
+#define MOUNT_PORT 4046
+/* handle 64 bit filehandles */
+#define FHSIZE3 64
+
+enum mountstat3 {
+    MNT3_OK = 0,                 /* no error */
+    MNT3ERR_PERM = 1,            /* Not owner */
+    MNT3ERR_NOENT = 2,           /* No such file or directory */
+    MNT3ERR_IO = 5,              /* I/O error */
+    MNT3ERR_ACCES = 13,          /* Permission denied */
+    MNT3ERR_NOTDIR = 20,         /* Not a directory */
+    MNT3ERR_INVAL = 22,          /* Invalid argument */
+    MNT3ERR_NAMETOOLONG = 63,    /* Filename too long */
+    MNT3ERR_NOTSUPP = 10004,     /* Operation not supported */
+    MNT3ERR_SERVERFAULT = 10006  /* A failure on the server */
+};
 
 typedef struct results {
     unsigned long us;
