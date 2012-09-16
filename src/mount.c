57835bbad0088efd336b729754de901bedca0aa2
Matt Provost <mprovost@wetafx.co.nz>
2012-09-16 21:37:01 +1200

    fsstat working

--- src/mount.c
+++ src/mount.c
@@ -2,10 +2,14 @@
 
 int main(int argc, char **argv) {
     mountres3 *mountres;
+    FSSTAT3res *fsstatres;
+    FSSTAT3args *fsstatargp;
     struct rpc_err clnt_err;
     int sock = RPC_ANYSOCK;
+    int nfs_sock = RPC_ANYSOCK;
     CLIENT *client;
     struct sockaddr_in client_sock;
+    struct sockaddr_in nfs_client_sock;
     struct timeval timeout = NFS_TIMEOUT;
     char *error;
     char hostname[INET_ADDRSTRLEN];
@@ -23,6 +27,7 @@ int main(int argc, char **argv) {
     inet_ntop(AF_INET, &client_sock.sin_addr, &hostname, INET_ADDRSTRLEN);
 
     client = clntudp_create(&client_sock, MOUNTPROG, version, timeout, &sock);
+    client->cl_auth = authunix_create_default();
 
     mountres = mountproc_mnt_3(&argv[2], client);
 
@@ -35,9 +40,23 @@ int main(int argc, char **argv) {
         printf("\n");
     }
 
-    clnt_destroy(client);
+    //clnt_destroy(client);
 
     client_sock.sin_port = htons(NFS_PORT);
-    client = clntudp_create(&client_sock, NFS_PROGRAM, version, timeout, &sock);
+    client = clntudp_create(&client_sock, NFS_PROGRAM, version, timeout, &nfs_sock);
+    client->cl_auth = authunix_create_default();
 
+    fsstatargp = calloc(1, sizeof(FSSTAT3args));
+    fsstatres = calloc(1, sizeof(FSSTAT3res));
+
+    fsstatargp->fsroot.data.data_len = mountres->mountres3_u.mountinfo.fhandle.fhandle3_len;
+    fsstatargp->fsroot.data.data_val = mountres->mountres3_u.mountinfo.fhandle.fhandle3_val;
+
+    fsstatres = nfsproc3_fsstat_3(fsstatargp, client);
+
+    if (fsstatres->status == NFS3_OK) {
+        printf("%llu bytes free\n", fsstatres->FSSTAT3res_u.resok.fbytes);
+    } else {
+        printf("oops\n");
+    }
 }
