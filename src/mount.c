04c4457333eadc6c279a96bc665b0ede9ed61e7b
Matt Provost <mprovost@wetafx.co.nz>
2012-10-05 18:51:01 +1300

    working dns!

--- src/mount.c
+++ src/mount.c
@@ -34,100 +34,106 @@ u_int mount_perror(mountstat3 fhs_status) {
     return fhs_status;
 }
 
+mountres3 *get_root_filehandle(struct sockaddr_in *client_sock, char *path) {
+    const u_long version = 3;
+    struct timeval timeout = NFS_TIMEOUT;
+    int sock = RPC_ANYSOCK;
+    CLIENT *client;
+    struct rpc_err clnt_err;
+    mountres3 *mountres;
+
+    /* get mount port from portmapper */
+    client_sock->sin_port = htons(pmap_getport(client_sock, MOUNTPROG, version, IPPROTO_UDP));
+    if (client_sock->sin_port == 0) {
+        clnt_pcreateerror("pmap_getport");
+        mountres->fhs_status = MNT3ERR_SERVERFAULT; /* is this the most appropriate error code? */
+        return mountres;
+    }
+
+    if (path[0] == '/') {
+        client = clntudp_create(client_sock, MOUNTPROG, version, timeout, &sock);
+        /* mounts don't need authentication because they return a list of authentication flavours supported */
+        client->cl_auth = authnone_create();
+
+        mountres = mountproc_mnt_3(&path, client);
+
+        if (mountres) {
+            if (mountres->fhs_status != MNT3_OK)
+                mount_perror(mountres->fhs_status);
+        } else {
+           clnt_geterr(client, &clnt_err);
+           /* check for authentication errors which probably mean it needs a low port */
+           if (clnt_err.re_status == RPC_AUTHERROR)
+               fprintf(stderr, "Unable to mount filesystem, consider running as root\n");
+
+           clnt_perror(client, "mountproc_mnt_3");
+        }
+    } else {
+        fprintf(stderr, "Invalid path: %s\n", path);
+        mountres->fhs_status = MNT3ERR_INVAL;
+    }
+        
+    clnt_destroy(client);
+    return mountres;
+}
+
 int main(int argc, char **argv) {
     mountres3 *mountres;
-    FSSTAT3res *fsstatres;
-    FSSTAT3args fsstatarg;
-    struct rpc_err clnt_err;
-    int sock = RPC_ANYSOCK;
-    int nfs_sock = RPC_ANYSOCK;
-    CLIENT *client;
     struct sockaddr_in client_sock;
-    struct sockaddr_in nfs_client_sock;
-    struct timeval timeout = NFS_TIMEOUT;
     char *error;
     char hostname[INET_ADDRSTRLEN];
     fhstatus result;
-    int intresult;
-    const u_long version = 3;
+    int getaddr;
+    struct addrinfo hints, *addr;
     bool_t dirpath;
     int i;
     char *host;
     char *path;
 
     client_sock.sin_family = AF_INET;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    /* default to UDP */
+    hints.ai_socktype = SOCK_DGRAM;
+
     host = strtok(argv[1], ":");
     path = strtok(NULL, ":");
-    intresult = inet_pton(AF_INET, host, &client_sock.sin_addr);
-    inet_ntop(AF_INET, &client_sock.sin_addr, &hostname, INET_ADDRSTRLEN);
-    printf("mounting %s on %s\n", path, hostname); 
 
-    /* get mount port from portmapper */
-    client_sock.sin_port = htons(pmap_getport(&client_sock, MOUNTPROG, version, IPPROTO_UDP));
-    if (client_sock.sin_port == 0) {
-        clnt_pcreateerror("pmap_getport");
-        exit(1);
-    }
+    /* first try treating the hostname as an IP address */
+    if (inet_pton(AF_INET, host, &client_sock.sin_addr)) {
+        mountres = get_root_filehandle(&client_sock, path);
+        if (mountres && mountres->fhs_status == MNT3_OK) {
+            printf("%s:", host);
+            for (i = 0; i < mountres->mountres3_u.mountinfo.fhandle.fhandle3_len; i++) {
+                printf("%02hhx", mountres->mountres3_u.mountinfo.fhandle.fhandle3_val[i]);
+            }
+            printf("\n");
+        }
+    } else {
+        /* if that fails, do a DNS lookup */
+        /* we don't call freeaddrinfo because we keep a pointer to the sin_addr in the client_sock */
+        getaddr = getaddrinfo(host, "nfs", &hints, &addr);
+        if (getaddr == 0) { /* success! */
+            /* loop through possibly multiple DNS responses */
+            while (addr) {
+                client_sock.sin_addr = ((struct sockaddr_in *)addr->ai_addr)->sin_addr;
+                inet_ntop(AF_INET, &((struct sockaddr_in *)addr->ai_addr)->sin_addr, hostname, INET_ADDRSTRLEN);
 
-    if (path[0] == '/') {
-        client = clntudp_create(&client_sock, MOUNTPROG, version, timeout, &sock);
-        client->cl_auth = authunix_create_default();
-
-        mountres = mountproc_mnt_3(&path, client);
-
-        if (mountres) {
-            if (mountres->fhs_status == MNT3_OK) {
-                printf("filehandle: ");
-                for (i = 0; i < mountres->mountres3_u.mountinfo.fhandle.fhandle3_len; i++) {
-                    printf("%02hhx", mountres->mountres3_u.mountinfo.fhandle.fhandle3_val[i]);
+                mountres = get_root_filehandle(&client_sock, path);
+                if (mountres && mountres->fhs_status == MNT3_OK) {
+                    printf("%s:", host);
+                    for (i = 0; i < mountres->mountres3_u.mountinfo.fhandle.fhandle3_len; i++) {
+                        printf("%02hhx", mountres->mountres3_u.mountinfo.fhandle.fhandle3_val[i]);
+                    }
+                    printf("\n");
                 }
-                printf("\n");
-            } else {
-                /* exit with the nonzero response */
-                exit(mount_perror(mountres->fhs_status));
+
+                addr = addr->ai_next;
             }
         } else {
-           clnt_geterr(client, &clnt_err);
-           /* check for authentication errors which probably mean it needs a low port */
-           if (clnt_err.re_status == RPC_AUTHERROR)
-               printf("Unable to mount filesystem, consider running as root\n");
-
-           clnt_perror(client, "mountproc_mnt_3");
-           exit(1);
+            fprintf(stderr, "%s: %s\n", host, gai_strerror(getaddr));
+            exit(EXIT_FAILURE);
         }
-
-        fsstatarg.fsroot.data.data_len = mountres->mountres3_u.mountinfo.fhandle.fhandle3_len;
-        fsstatarg.fsroot.data.data_val = mountres->mountres3_u.mountinfo.fhandle.fhandle3_val;
-
-        //clnt_destroy(client);
-    } else {
-        /* hex takes two characters for each byte */
-        if (strlen(path) == FHSIZE * 2) {
-            fsstatarg.fsroot.data.data_val = calloc(FHSIZE, sizeof(char));
-            for (i = 0; i < FHSIZE; i++)
-                sscanf(&path[i * 2], "%2hhx", &fsstatarg.fsroot.data.data_val[i]);
-
-                printf("filehandle hex: ");
-                for (i = 0; i < 32; i++) {
-                    printf("%02hhx", fsstatarg.fsroot.data.data_val[i]);
-                }
-                printf("\n");
-        fsstatarg.fsroot.data.data_len = FHSIZE;
-        } else {
-            printf("oops! %zi\n", strlen(path));
-        }
-    }
-        
-    client_sock.sin_port = htons(NFS_PORT);
-    client = clntudp_create(&client_sock, NFS_PROGRAM, version, timeout, &nfs_sock);
-    client->cl_auth = authunix_create_default();
-
-    fsstatres = calloc(1, sizeof(FSSTAT3res));
-    fsstatres = nfsproc3_fsstat_3(&fsstatarg, client);
-
-    if (fsstatres->status == NFS3_OK) {
-        printf("%llu bytes free\n", fsstatres->FSSTAT3res_u.resok.fbytes);
-    } else {
-        printf("oops\n");
     }
 }
