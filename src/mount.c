19162a3376c0b385afa30b1e82ea7dcb15d2ba9a
Matt Provost <mprovost@wetafx.co.nz>
2012-11-29 18:25:44 +1300

    refactor client setup out of get_root_filehandle so we can reuse the client

--- src/mount.c
+++ src/mount.c
@@ -45,63 +45,54 @@ u_int mount_perror(mountstat3 fhs_status) {
 }
 
 
-mountres3 *get_root_filehandle(char *hostname, struct sockaddr_in *client_sock, int socktype, char *path) {
-    struct timeval timeout = NFS_TIMEOUT;
-    int sock = RPC_ANYSOCK;
-    CLIENT *client;
+mountres3 *get_root_filehandle(char *hostname, CLIENT *client, char *path) {
     struct rpc_err clnt_err;
     mountres3 *mountres;
-    u_long version = 3;
+    exports *ex;
+    dirpath dp;
     int i;
 
     if (path[0] == '/') {
-        /* get mount port from portmapper */
-        client_sock->sin_port = htons(pmap_getport(client_sock, MOUNTPROG, version, IPPROTO_UDP));
-        if (client_sock->sin_port) {
-            if (socktype == SOCK_DGRAM)
-                client = clntudp_create(client_sock, MOUNTPROG, version, timeout, &sock);
-            else
-                client = clnttcp_create(client_sock, MOUNTPROG, version, &sock, 0, 0);
-                
-            /* mounts don't need authentication because they return a list of authentication flavours supported */
-            client->cl_auth = authnone_create();
+        /* the actual RPC call */
+        mountres = mountproc_mnt_3(&path, client);
 
-            /* the actual RPC call */
-            mountres = mountproc_mnt_3(&path, client);
+        ex = mountproc_export_3(NULL, client);
 
-            if (mountres) {
-                if (mountres->fhs_status == MNT3_OK) {
-                    printf("%s:%s:", hostname, path);
-                    /* print the filehandle in hex */
-                    for (i = 0; i < mountres->mountres3_u.mountinfo.fhandle.fhandle3_len; i++) {
-                        printf("%02hhx", mountres->mountres3_u.mountinfo.fhandle.fhandle3_val[i]);
-                    }
-                    printf("\n");
-                } else {
-                    fprintf(stderr, "%s:%s ", hostname, path);
-                    mount_perror(mountres->fhs_status);
+        /* ugh what is up with these pointers? */
+        do {
+            printf("dirpath = %s\n", (*(ex))->ex_dir);
+            ex = &(*(ex))->ex_next;
+        } while ((*(ex))->ex_next);
+
+        if (mountres) {
+            if (mountres->fhs_status == MNT3_OK) {
+                printf("%s:%s:", hostname, path);
+                /* print the filehandle in hex */
+                for (i = 0; i < mountres->mountres3_u.mountinfo.fhandle.fhandle3_len; i++) {
+                    printf("%02hhx", mountres->mountres3_u.mountinfo.fhandle.fhandle3_val[i]);
                 }
+                printf("\n");
             } else {
                 fprintf(stderr, "%s:%s ", hostname, path);
-                clnt_geterr(client, &clnt_err);
-                /* check for authentication errors which probably mean it needs to come from a low port */
-                if (clnt_err.re_status == RPC_AUTHERROR)
-                   fprintf(stderr, "Unable to mount filesystem, consider running as root\n");
-
-                clnt_perror(client, "mountproc_mnt_3");
+                mount_perror(mountres->fhs_status);
             }
         } else {
-            clnt_pcreateerror("pmap_getport");
-            mountres->fhs_status = MNT3ERR_SERVERFAULT; /* is this the most appropriate error code? */
+            fprintf(stderr, "%s:%s ", hostname, path);
+            clnt_geterr(client, &clnt_err);
+            /* check for authentication errors which probably mean it needs to come from a low port */
+            if (clnt_err.re_status == RPC_AUTHERROR)
+               fprintf(stderr, "Unable to mount filesystem, consider running as root\n");
+
+            clnt_perror(client, "mountproc_mnt_3");
         }
     } else {
         fprintf(stderr, "%s: Invalid path: %s\n", hostname, path);
         mountres->fhs_status = MNT3ERR_INVAL;
     }
 
-    clnt_destroy(client);
     return mountres;
 }
+
 
 int main(int argc, char **argv) {
     mountres3 *mountres;
@@ -111,10 +102,13 @@ int main(int argc, char **argv) {
     fhstatus result;
     int getaddr;
     struct addrinfo hints, *addr;
-    bool_t dirpath;
     char *host;
     char *path;
     int ch, first, index;
+    CLIENT *client;
+    u_long version = 3;
+    struct timeval timeout = NFS_TIMEOUT;
+    int sock = RPC_ANYSOCK;
 
     client_sock.sin_family = AF_INET;
 
@@ -143,32 +137,47 @@ int main(int argc, char **argv) {
     host = strtok(argv[optind], ":");
     path = strtok(NULL, ":");
 
-    /* first try treating the hostname as an IP address */
-    if (inet_pton(AF_INET, host, &client_sock.sin_addr)) {
-        mountres = get_root_filehandle(host, &client_sock, hints.ai_socktype, path);
-    } else {
-        /* if that fails, do a DNS lookup */
-        /* we don't call freeaddrinfo because we keep a pointer to the sin_addr in the client_sock */
-        getaddr = getaddrinfo(host, "nfs", &hints, &addr);
-        if (getaddr == 0) { /* success! */
-            /* loop through possibly multiple DNS responses */
-            while (addr) {
-                client_sock.sin_addr = ((struct sockaddr_in *)addr->ai_addr)->sin_addr;
+    /* DNS lookup */
+    getaddr = getaddrinfo(host, "nfs", &hints, &addr);
+    if (getaddr == 0) { /* success! */
+        /* loop through possibly multiple DNS responses */
+        while (addr) {
+            client_sock.sin_addr = ((struct sockaddr_in *)addr->ai_addr)->sin_addr;
 
-                if (inet_ntop(AF_INET, &((struct sockaddr_in *)addr->ai_addr)->sin_addr, hostname, INET_ADDRSTRLEN)) {
-                    mountres = get_root_filehandle(hostname, &client_sock, hints.ai_socktype, path);
-                } else {
-                    fprintf(stderr, "%s: ", hostname);
-                    perror("inet_ntop");
-                    return EXIT_FAILURE;
-                }
-
-                addr = addr->ai_next;
+            /* get mount port from portmapper */
+            if (hints.ai_socktype == SOCK_DGRAM) {
+                client_sock.sin_port = htons(pmap_getport(&client_sock, MOUNTPROG, version, IPPROTO_UDP));
+                /* TODO check sin_port */
+                client = clntudp_create(&client_sock, MOUNTPROG, version, timeout, &sock);
+            } else {
+                client_sock.sin_port = htons(pmap_getport(&client_sock, MOUNTPROG, version, IPPROTO_TCP));
+                /* TODO check sin_port */
+                client = clnttcp_create(&client_sock, MOUNTPROG, version, &sock, 0, 0);
             }
-        } else {
-            fprintf(stderr, "%s: %s\n", host, gai_strerror(getaddr));
-            return EXIT_FAILURE;
+
+            if (client_sock.sin_port == 0) {
+                clnt_pcreateerror("pmap_getport");
+                mountres->fhs_status = MNT3ERR_SERVERFAULT; /* is this the most appropriate error code? */
+            }
+                    
+            /* mounts don't need authentication because they return a list of authentication flavours supported */
+            client->cl_auth = authnone_create();
+
+            if (inet_ntop(AF_INET, &((struct sockaddr_in *)addr->ai_addr)->sin_addr, hostname, INET_ADDRSTRLEN)) {
+                mountres = get_root_filehandle(hostname, client, path);
+            } else {
+                fprintf(stderr, "%s: ", hostname);
+                perror("inet_ntop");
+                return EXIT_FAILURE;
+            }
+
+            clnt_destroy(client);
+
+            addr = addr->ai_next;
         }
+    } else {
+        fprintf(stderr, "%s: %s\n", host, gai_strerror(getaddr));
+        return EXIT_FAILURE;
     }
 
     if (mountres)
