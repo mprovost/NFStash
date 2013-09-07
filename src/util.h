ac70e453bfd3861ef7731c77cf5fed189288b4e2
Matt Provost <mprovost@wetafx.co.nz>
2013-09-07 22:45:51 +1200

    include guard

--- src/util.h
+++ src/util.h
@@ -1,4 +1,7 @@
-#include <nfsping.h>
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "nfsping.h"
 
 size_t parse_fh(char *, fsroots_t **);
 char* reverse_fqdn(char *);
@@ -7,3 +10,5 @@ unsigned long tv2ms(struct timeval);
 void ms2tv(struct timeval *, unsigned long);
 void ms2ts(struct timespec *, unsigned long);
 unsigned long ts2ms(struct timespec);
+
+#endif /* UTIL_H */
