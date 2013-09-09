755aecd156f045adc82f18c5a76f909ca043a259
Matt Provost <mprovost@wetafx.co.nz>
2013-09-09 18:02:06 +1200

    use rpc.c and create_rpc_client

--- src/cat.c
+++ src/cat.c
@@ -3,13 +3,10 @@
 
 
 /* the NFS READ call */
-unsigned int do_read(fsroots_t *dir) {
+unsigned int do_read(fsroots_t *dir, struct addrinfo *hints, uint16_t port, unsigned long version, struct timeval timeout) {
     READ3res *res;
     READ3args args;
     CLIENT *client;
-    const u_long version = 3;
-    struct timeval timeout = NFS_TIMEOUT;
-    int nfs_sock = RPC_ANYSOCK;
     struct rpc_err clnt_err;
     int i;
     unsigned int count = 0;
@@ -24,16 +21,14 @@ unsigned int do_read(fsroots_t *dir) {
     dir->client_sock->sin_port = htons(NFS_PORT);
 
     //client = *clntudp_create(dir->client_sock, NFS_PROGRAM, version, timeout, &nfs_sock);
+    client = create_rpc_client(dir->client_sock, hints, port, NFS_PROGRAM, version, timeout);
+    client->cl_auth = authunix_create_default();
 
     args.file = dir->fsroot;
     args.offset = 0;
     args.count = 8192;
 
     do {
-        nfs_sock = RPC_ANYSOCK;
-        client = clnttcp_create(dir->client_sock, NFS_PROGRAM, version, &nfs_sock, 0, 0);
-        client->cl_auth = authunix_create_default();
-
         gettimeofday(&call_start, NULL);
         res = nfsproc3_read_3(&args, client);
         gettimeofday(&call_end, NULL);
@@ -77,9 +72,6 @@ unsigned int do_read(fsroots_t *dir) {
         } else {
             clnt_perror(client, "nfsproc3_read_3");
         }
-            auth_destroy(client->cl_auth);
-            clnt_destroy(client);
-            close(nfs_sock);
     } while (res);
 
     return count;
@@ -90,9 +82,17 @@ int main(int argc, char **argv) {
     char input_fh[FHMAX];
     fsroots_t *current, *tail, dummy;
     READ3res *res;
+    struct addrinfo hints;
+    uint16_t port = htons(NFS_PORT);
+    unsigned long version = 3;
+    struct timeval timeout = NFS_TIMEOUT;
 
     dummy.next = NULL;
     tail = &dummy;
+
+    hints.ai_family = AF_INET;
+    /* default to UDP */
+    hints.ai_socktype = SOCK_DGRAM;
 
     while (fgets(input_fh, FHMAX, stdin)) {
         tail->next = malloc(sizeof(fsroots_t));
@@ -105,7 +105,7 @@ int main(int argc, char **argv) {
     /* skip the first empty struct */
     current = dummy.next;
     while (current) {
-        do_read(current);
+        do_read(current, &hints, port, version, timeout);
         current = current->next;
     }
 }
