1cc8d53822042ed44a74ca4bcf48b8e59befa1e6
Matt Provost <mprovost@wetafx.co.nz>
2013-12-09 22:53:07 +1300

    new function print_fh to print out NFS filehandles in hex

--- src/util.h
+++ src/util.h
@@ -4,6 +4,7 @@
 #include "nfsping.h"
 
 size_t parse_fh(char *, fsroots_t **);
+int print_fh(char *, char *, fhandle3);
 char* reverse_fqdn(char *);
 unsigned long tv2us(struct timeval);
 unsigned long tv2ms(struct timeval);
