f8db74c3ed1a30f5b6ebbdbaec0c142a298617cb
Matt Provost <mprovost@termcap.net>
2014-05-24 21:41:13 +0000

    Linux denies root access as a mount protocol error, not RPC

--- src/mount.c
+++ src/mount.c
@@ -61,7 +61,11 @@ mountres3 *get_root_filehandle(char *hostname, CLIENT *client, char *path) {
         if (mountres) {
             if (mountres->fhs_status != MNT3_OK) {
                 fprintf(stderr, "%s:%s: ", hostname, path);
-                mount_perror(mountres->fhs_status);
+				if (mountres->fhs_status == MNT3ERR_ACCES) {
+					fprintf(stderr, "Unable to mount filesystem, consider running as root\n");
+				} else {
+					mount_perror(mountres->fhs_status);
+				}
             }
         } else {
             fprintf(stderr, "%s:%s: ", hostname, path);
