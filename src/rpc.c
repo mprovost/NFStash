239a5a6b484854ff03d460f1936f84e505bf9f05
Matt Provost <mprovost@termcap.net>
2015-07-20 22:08:12 +0000

    in create_rpc_client(), check if we get a zero response from the portmapper

--- src/rpc.c
+++ src/rpc.c
@@ -47,7 +47,7 @@ CLIENT *destroy_rpc_client(CLIENT *client) {
 /* takes an initialised sockaddr_in with the address and port */
 /* returns an initialised client, or NULL on error */
 CLIENT *create_rpc_client(struct sockaddr_in *client_sock, struct addrinfo *hints, unsigned long prognum, unsigned long version, struct timeval timeout, struct sockaddr_in src_ip) {
-    CLIENT *client;
+    CLIENT *client = NULL;
     int sock;
     long unsigned protocol; /* for portmapper */
     char ip[INET_ADDRSTRLEN];
@@ -122,55 +122,58 @@ CLIENT *create_rpc_client(struct sockaddr_in *client_sock, struct addrinfo *hint
 
     /* now make the client connection */
 
-    /* Make sure and make new sockets for each new connection */
-    /* clnttcp_create will happily reuse open sockets */
-    sock = socket(AF_INET, hints->ai_socktype, 0);
-    if (sock < 0) {
-        perror("create_rpc_client(socket)");
-        return NULL;
-    }
-
-    /* always try and bind to a low port first */
-    /* could check for root here but there are other mechanisms for allowing processes to bind to low ports */
-    if (bindresvport(sock, &src_ip) == -1) {
-        /* permission denied, ie we aren't root */
-        if (errno == EACCES) {
-            /* try an ephemeral port */
-            src_ip.sin_port = htons(0);
-        } else {
-            perror("create_rpc_client(bindresvport)");
+    /* by now we should have a port defined no matter what */
+    if (client_sock->sin_port) {
+        /* Make sure and make new sockets for each new connection */
+        /* clnttcp_create will happily reuse open sockets */
+        sock = socket(AF_INET, hints->ai_socktype, 0);
+        if (sock < 0) {
+            perror("create_rpc_client(socket)");
             return NULL;
         }
-    }
 
-    /* now we're bound to a local socket, try and connect to the server */
-    if (connect(sock, (struct sockaddr *)client_sock, sizeof(struct sockaddr)) == 0) {
-        /* TCP */
-        if (hints->ai_socktype == SOCK_STREAM) {
-                /* TODO set recvsz and sendsz to the NFS blocksize */
-                client = clnttcp_create(client_sock, prognum, version, &sock, 0, 0);
-                if (client == NULL) {
-                    clnt_pcreateerror("clnttcp_create");
-                }
-        /* UDP */
-        } else {
-            client = clntudp_create(client_sock, prognum, version, timeout, &sock);
-            if (client == NULL) {
-                clnt_pcreateerror("clntudp_create");
+        /* always try and bind to a low port first */
+        /* could check for root here but there are other mechanisms for allowing processes to bind to low ports */
+        if (bindresvport(sock, &src_ip) == -1) {
+            /* permission denied, ie we aren't root */
+            if (errno == EACCES) {
+                /* try an ephemeral port */
+                src_ip.sin_port = htons(0);
+            } else {
+                perror("create_rpc_client(bindresvport)");
+                return NULL;
             }
         }
-    } else {
-        perror("create_rpc_client(connect)");
-        return NULL;
-    }
 
-    if (verbose) {
-        if (getsockname(sock, (struct sockaddr *)&getaddr, &len) == -1) {
-            perror("create_rpc_client(getsockname)");
-            /* this is just verbose output so don't return an error */
+        /* now we're bound to a local socket, try and connect to the server */
+        if (connect(sock, (struct sockaddr *)client_sock, sizeof(struct sockaddr)) == 0) {
+            /* TCP */
+            if (hints->ai_socktype == SOCK_STREAM) {
+                    /* TODO set recvsz and sendsz to the NFS blocksize */
+                    client = clnttcp_create(client_sock, prognum, version, &sock, 0, 0);
+                    if (client == NULL) {
+                        clnt_pcreateerror("clnttcp_create");
+                    }
+            /* UDP */
+            } else {
+                client = clntudp_create(client_sock, prognum, version, timeout, &sock);
+                if (client == NULL) {
+                    clnt_pcreateerror("clntudp_create");
+                }
+            }
         } else {
-            inet_ntop(AF_INET, (struct sockaddr_in *)&getaddr.sin_addr, ip, INET_ADDRSTRLEN);
-            debug("source port = %s:%u\n", ip, ntohs(getaddr.sin_port));
+            perror("create_rpc_client(connect)");
+            return NULL;
+        }
+
+        if (verbose) {
+            if (getsockname(sock, (struct sockaddr *)&getaddr, &len) == -1) {
+                perror("create_rpc_client(getsockname)");
+                /* this is just verbose output so don't return an error */
+            } else {
+                inet_ntop(AF_INET, (struct sockaddr_in *)&getaddr.sin_addr, ip, INET_ADDRSTRLEN);
+                debug("source port = %s:%u\n", ip, ntohs(getaddr.sin_port));
+            }
         }
     }
 
