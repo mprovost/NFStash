b3d5b940ff0417d3ea5cb911f87bd6ee03e7f3e5
Matt Provost <mprovost@termcap.net>
2016-01-19 10:12:47 +0000

    use make_exports() to build a target list if no path given

--- src/mount.c
+++ src/mount.c
@@ -105,55 +105,47 @@ mountres3 *get_root_filehandle(CLIENT *client, char *hostname, char *path, unsig
     mountres3 *mountres = NULL;
     struct timespec call_start, call_end, call_elapsed;
 
-    /* TODO check for valid path when parsing arguments in main() */
-    if (path[0] == '/') {
-        if (client) {
-            /* first time marker */
+    if (client) {
+        /* first time marker */
 #ifdef CLOCK_MONOTONIC_RAW
-            clock_gettime(CLOCK_MONOTONIC_RAW, &call_start);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &call_start);
 #else
-            clock_gettime(CLOCK_MONOTONIC, &call_start);
+        clock_gettime(CLOCK_MONOTONIC, &call_start);
 #endif
 
-            /* the actual RPC call */
-            mountres = mountproc_mnt_3(&path, client);
+        /* the actual RPC call */
+        mountres = mountproc_mnt_3(&path, client);
 
-            /* second time marker */
+        /* second time marker */
 #ifdef CLOCK_MONOTONIC_RAW
-            clock_gettime(CLOCK_MONOTONIC_RAW, &call_end);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &call_end);
 #else
-            clock_gettime(CLOCK_MONOTONIC, &call_end);
+        clock_gettime(CLOCK_MONOTONIC, &call_end);
 #endif
 
-            /* calculate elapsed microseconds */
-            timespecsub(&call_end, &call_start, &call_elapsed);
-            *usec = ts2us(call_elapsed);
-        }
+        /* calculate elapsed microseconds */
+        timespecsub(&call_end, &call_start, &call_elapsed);
+        *usec = ts2us(call_elapsed);
+    }
 
-        if (mountres) {
-            if (mountres->fhs_status != MNT3_OK) {
-                fprintf(stderr, "%s:%s: ", hostname, path);
-                /* check if we get an access error, this probably means the server wants us to use a reserved port */
-                /* TODO do this check in mount_perror? */
-                if (mountres->fhs_status == MNT3ERR_ACCES && geteuid()) {
-                    fprintf(stderr, "Unable to mount filesystem, consider running as root\n");
-                } else {
-                    mount_perror(mountres->fhs_status);
-                }
-            }
-        /* RPC error */
-        } else {
-            clnt_geterr(client, &clnt_err);
-            if  (clnt_err.re_status) {
-                fprintf(stderr, "%s:%s: ", hostname, path);
-                clnt_perror(client, "mountproc_mnt_3");
+    if (mountres) {
+        if (mountres->fhs_status != MNT3_OK) {
+            fprintf(stderr, "%s:%s: ", hostname, path);
+            /* check if we get an access error, this probably means the server wants us to use a reserved port */
+            /* TODO do this check in mount_perror? */
+            if (mountres->fhs_status == MNT3ERR_ACCES && geteuid()) {
+                fprintf(stderr, "Unable to mount filesystem, consider running as root\n");
+            } else {
+                mount_perror(mountres->fhs_status);
             }
         }
+    /* RPC error */
     } else {
-        fprintf(stderr, "%s: Invalid path: %s\n", hostname, path);
-        /* create an empty result */
-        mountres = malloc(sizeof(mountres));
-        mountres->fhs_status = MNT3ERR_INVAL;
+        clnt_geterr(client, &clnt_err);
+        if  (clnt_err.re_status) {
+            fprintf(stderr, "%s:%s: ", hostname, path);
+            clnt_perror(client, "mountproc_mnt_3");
+        }
     }
 
     return mountres;
@@ -201,34 +193,38 @@ int print_exports(char *host, struct exportnode *ex) {
 }
 
 
+/* make a target list by querying the server for a list of exports */
 targets_t *make_exports(targets_t *target, const uint16_t port) {
     exports ex;
+    /* timer for the exports call */
     unsigned long usec = 0;
+    targets_t dummy;
+    targets_t *current = &dummy;
+    targets_t *head = current;
 
-    /* get the list of exports from the server */
-    ex = get_exports(target->client, target->name, &usec);
-    printf("get_exports: %lu\n", usec);
+    if (target->client) {
+        /* get the list of exports from the server */
+        ex = get_exports(target->client, target->name, &usec);
+        printf("get_exports: %lu\n", usec);
 
-    while (ex) {
-        /* copy the hostname from the mount result into the target */
-        target->path = calloc(1, MNTPATHLEN);
-        strncpy(target->path, ex->ex_dir, MNTPATHLEN);
+        while (ex) {
+            /* allocate a new entry */
+            current->next = init_target(target->name, port);
+            current = current->next;
+            /* copy the hostname from the mount result into the target */
+            current->path = calloc(1, MNTPATHLEN);
+            strncpy(current->path, ex->ex_dir, MNTPATHLEN);
+            /* reuse the same client connection for each export */
+            current->client = target->client;
+            /* and the same client_sock */
+            current->client_sock = target->client_sock;
 
-        /* if there's another result, make a new target */
-        if (ex->ex_next) {
-            /* make a new target */
-            target->next = init_target(target->name, port);
-
-            /* reuse the same client connection */
-            target->next->client = target->client;
-
-            target = target->next;
+            ex = ex->ex_next;
         }
-
-        ex = ex->ex_next;
     }
 
-    return target;
+    /* skip the dummy entry */
+    return head->next;
 }
 
 
@@ -241,9 +237,13 @@ int main(int argc, char **argv) {
     };
     char *host;
     char *path;
+    exports ex;
     targets_t *targets;
     targets_t *current;
-    targets_t target_dummy;
+    targets_t target_dummy = {0};
+    targets_t *new_targets;
+    targets_t exports_dummy = {0};
+    targets_t *exports_dummy_ptr = &exports_dummy;
     unsigned int exports_count = 0, exports_ok = 0;
     int ch;
     /* command line options */
@@ -318,25 +318,50 @@ int main(int argc, char **argv) {
         host = strtok(argv[optind], ":");
         path = strtok(NULL, ":");
 
-        if (path && showmount) {
-            fatalx(3, "Can't specify -e (exports) and a path!\n");
-        }
-
-        current->next = make_target(host, &hints, port, dns, ip, multiple);
-
         if (path) {
-            current->path = path;
-        } else {
-            /* create an rpc connection */
-            if (current->next->client == NULL) {
-                current->next->client = create_rpc_client(current->next->client_sock, &hints, MOUNTPROG, version, timeout, src_ip);
+            /* check for valid path */
+            if (path[0] != '/') {
+                fatalx(3, "%s: Invalid path: %s\n", host, path);
             }
 
-            /* look up the export list on the server and create a target for each */
-            current = make_exports(current->next, port);
+            if (showmount) {
+                fatalx(3, "Can't specify -e (exports) and a path!\n");
+            }
         }
 
-        current = current->next;
+        /* make possibly multiple new targets */
+        new_targets = make_target(host, &hints, port, dns, ip, multiple);
+
+        /* go through this argument's list of possibly multiple dns responses/targets */
+
+        while (new_targets) {
+            if (path) {
+                new_targets->path = path;
+            /* no path given, look up exports on server */
+            } else {
+                /* first create an rpc connection so we can query the server for an exports list */
+                new_targets->client = create_rpc_client(new_targets->client_sock, &hints, MOUNTPROG, version, timeout, src_ip);
+
+                if (showmount) {
+                    ex = get_exports(new_targets->client, host, &usec);
+                    exports_count = print_exports(host, ex);
+                } else {
+                    /* look up the export list on the server and create a target for each */
+                    append_target(&exports_dummy_ptr, make_exports(new_targets, port));
+                }
+            }
+
+            new_targets = new_targets->next;
+        }
+
+        /* append to the global target list */
+        if (path) {
+            append_target(&targets, new_targets);
+        } else {
+            /* TODO showmount */
+            /* skip the dummy entry */
+            append_target(&targets, exports_dummy.next);
+        }
 
         optind++;
     }
@@ -346,7 +371,11 @@ int main(int argc, char **argv) {
     signal(SIGINT, int_handler);
 
     /* skip the first dummy entry */
-    targets = targets->next;
+    if (showmount) {
+        targets = NULL;
+    } else {
+        targets = targets->next;
+    }
 
     while(1) {
         /* reset to head of list */
