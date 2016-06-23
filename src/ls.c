349c66e8a77c22e9ad4b375dcf065eb992f4f036
Matt Provost <mprovost@termcap.net>
2016-06-23 08:42:00 +0000

    change nfsls to use the new target/filehandle structs

--- src/ls.c
+++ src/ls.c
@@ -4,7 +4,7 @@
 
 /* local prototypes */
 static void usage(void);
-static entryplus3 *do_readdirplus(CLIENT *, nfs_fh_list *);
+static entryplus3 *do_readdirplus(CLIENT *, char *, char *, nfs_fh3);
 
 /* globals */
 int verbose = 0;
@@ -22,11 +22,11 @@ void usage() {
 
 
 /* do readdirplus calls to get a full list of directory entries */
-entryplus3 *do_readdirplus(CLIENT *client, nfs_fh_list *dir) {
+entryplus3 *do_readdirplus(CLIENT *client, char *host, char *path, nfs_fh3 dir) {
     READDIRPLUS3res *res;
     entryplus3 *res_entry, *current, dummy;
     READDIRPLUS3args args = {
-        .dir = dir->nfs_fh,
+        .dir = dir,
         .cookie = 0,
         .dircount = 512,
         .maxcount = 8192,
@@ -62,7 +62,7 @@ entryplus3 *do_readdirplus(CLIENT *client, nfs_fh_list *dir) {
                     res = nfsproc3_readdirplus_3(&args, client);
                 }
             } else {
-                fprintf(stderr, "%s:%s: ", dir->host, dir->path);
+                fprintf(stderr, "%s:%s: ", host, path);
                 clnt_geterr(client, &clnt_err);
                 if (clnt_err.re_status)
                     clnt_perror(client, "nfsproc3_readdirplus_3");
@@ -86,13 +86,15 @@ int main(int argc, char **argv) {
     char   *input_fh  = NULL;
     size_t  input_len = 0;
     char *file_name;
-    nfs_fh_list *current;
+    targets_t dummy = { 0 };
+    targets_t *targets = &dummy;
+    targets_t *current;
+    struct nfs_fh_list *filehandle;
     struct addrinfo hints = {
         .ai_family = AF_INET,
         /* default to UDP */
         .ai_socktype = SOCK_DGRAM,
     };
-    CLIENT *client = NULL;
     struct sockaddr_in clnt_info;
     unsigned long version = 3;
     struct timeval timeout = NFS_TIMEOUT;
@@ -130,40 +132,27 @@ int main(int argc, char **argv) {
     }
 
     /* no arguments, use stdin */
-    if (optind == argc) {
-        if (getline(&input_fh, &input_len, stdin) == -1) {
-            input_fh = NULL;
-        }
-    /* first argument */
-    } else {
-        input_fh = argv[optind];
+    while (getline(&input_fh, &input_len, stdin) != -1) {
+        current = parse_fh(targets, input_fh, 0, 0, ping);
     }
 
-    while (input_fh) {
+    /* skip the dummy entry */
+    targets = targets->next;
+    current = targets;
 
-        current = parse_fh(input_fh);
+    while (current) {
+        if (current->client == NULL) {
+            /* connect to server */
+            current->client = create_rpc_client(current->client_sock, &hints, NFS_PROGRAM, version, timeout, src_ip);
+            auth_destroy(current->client->cl_auth);
+            current->client->cl_auth = authunix_create_default();
+        }
 
-        if (current) {
-            /* check if we can use the same client connection as the previous target */
-            /* get the server address out of the client */
-            if (client) {
-                clnt_control(client, CLGET_SERVER_ADDR, (char *)&clnt_info);
-                if (clnt_info.sin_addr.s_addr != current->client_sock->sin_addr.s_addr) {
-                    client = destroy_rpc_client(client);
-                }
-            }
+        if (current->client) {
+            filehandle = current->filehandles;
 
-            if (client == NULL) {
-                current->client_sock->sin_family = AF_INET;
-                current->client_sock->sin_port = htons(NFS_PORT);
-                /* connect to server */
-                client = create_rpc_client(current->client_sock, &hints, NFS_PROGRAM, version, timeout, src_ip);
-                auth_destroy(client->cl_auth);
-                client->cl_auth = authunix_create_default();
-            }
-
-            if (client) {
-                entries = do_readdirplus(client, current);
+            while (filehandle) {
+                entries = do_readdirplus(current->client, current->name, filehandle->path, filehandle->nfs_fh);
 
                 while (entries) {
                     count++;
@@ -189,33 +178,21 @@ int main(int argc, char **argv) {
                             file_name = entries->name;
                         }
 
-                        print_nfs_fh3((struct sockaddr *)current->client_sock, current->path, file_name, entries->name_handle.post_op_fh3_u.handle);
+                        print_nfs_fh3((struct sockaddr *)current->client_sock, filehandle->path, file_name, entries->name_handle.post_op_fh3_u.handle);
 
                         /* TODO free(file_name) */
                     }
 
                     entries = entries->nextentry;
                 }
-            }
-            /* cleanup */
-            free(current->client_sock);
-            free(current);
+
+                filehandle = filehandle->next;
+            } /* while (filehandle) */
         }
 
-        /* get the next filehandle*/
-        if (optind == argc) {
-            if (getline(&input_fh, &input_len, stdin) == -1) {
-                input_fh = NULL;
-            }
-        } else {
-            optind++;
-            if (optind < argc) {
-                input_fh = argv[optind];
-            } else {
-                input_fh = NULL;
-            }
-        }
-    }
+        current = current->next;
+    } /* while (current) */
+
 
     /* return success if we saw any entries */
     /* TODO something better! */
