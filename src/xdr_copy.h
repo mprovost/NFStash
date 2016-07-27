4e9d975e1f8de8e9c5cc818cc3aa4f571399351f
Matt Provost <mprovost@termcap.net>
2016-07-27 10:16:41 +0000

    only need xdr.h for xdrproc_t

--- src/xdr_copy.h
+++ src/xdr_copy.h
@@ -1,6 +1,5 @@
 /* from http://stackoverflow.com/questions/27000237/does-this-rpc-xdr-copy-make-sense */
 
-#include <rpc/rpc.h>
 #include <rpc/xdr.h>
 
 #define XDR_COPY( T, d, s  ) xdr_copy_(( xdrproc_t  )xdr_##T, ( char*  )d, ( char*  )s, sizeof( T  ))
